f113b47ca4a150460daff0b27b27f16abd778b6b
iserrano <iserrano@mills.edu>
2020-12-08 17:59:18 -0800

    Setup

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,49 +1,42 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Objects;
 
 public class ContactList {
-    private static int hashTableSize = 0;
-    private static HashMap<String, Contact> contactList;
+    private static int hashTableSize = 19; // initial size of table
+    private static ContactNode[] hashTable; // hash table
+    private ContactNode head;
+    private int numContacts = 0;// contact counter
+
+    private class ContactNode {
+        public String key;
+        public Contact value;
+        public ContactNode next;
+    }
 
     public ContactList() {
-        contactList = new HashMap<String, Contact>();
+        hashTable = new ContactNode[hashTableSize];
+        // when creating a new hash table, the reference to a ContactNode should be null
+    }
+
+    private int hashValue(String s) {
+        int value = Objects.hash(s.charAt(0));
+        int index = value % hashTableSize;
+        return index;
     }
 
     // run time: O(1)
     public boolean insert(String name, String number) // no duplicates
     {
-        // create new contact every time you
-        // want to insert a person into the hash-map
-        Contact object = new Contact(name, number);
-
-        contactList.put(number, object);
-        contactList.put(name, object);
-
-        hashTableSize++;
         return true;
     }
 
     // run time: O(1)
     public boolean delete(String nameORnumber) {
-        if(contactList.containsKey(nameORnumber)) {// checking if item exists
-            if (isNumeric(nameORnumber)) { // deleting other instance before search instance
-                contactList.remove(contactList.get(nameORnumber).getName());
-            }else {
-                contactList.remove(contactList.get(nameORnumber).getNumber());
-            }
-            contactList.remove(nameORnumber); // deleting search instance
-            hashTableSize--;
-            return true;
-        }
-
         return false;
     }
 
     // run time: O(1)
     public String find(String nameORnumber) {
-        if (contactList.containsKey(nameORnumber))
-            return contactList.get(nameORnumber).toString();
+
         return "Contact not found";
     }
 
@@ -54,22 +47,12 @@ public class ContactList {
 
     // run time: O(N log N)
     public void printAllContacts() {
-        ArrayList<String> sortedContacts = new ArrayList<>(contactList.keySet());
-        Collections.sort(sortedContacts);
-        for (String s : sortedContacts) {
-            if (!isNumeric(s))
-                System.out.println(contactList.get(s).toString());
-        }
+
     }
 
     // run time: O(N log N)
     public void searchAllContacts(String target) {
-        ArrayList<String> sortedContacts = new ArrayList<>(contactList.keySet());
-        Collections.sort(sortedContacts);
-        for (String s : sortedContacts) {
-            if (s.contains(target))
-                System.out.println(contactList.get(s).toString());
-        }
+
     }
 
     private static boolean isNumeric(String strNum) {
