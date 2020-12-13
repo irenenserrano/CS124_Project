a452f09f97e620e29628b9585e540a5a12274d0f
iserrano <iserrano@mills.edu>
2020-12-13 13:54:20 -0800

    Finish implementing tests

--- test/ContactListTest.java
+++ test/ContactListTest.java
@@ -1,75 +1,98 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.stream.Stream;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
-public class ContactListTest<AnyType extends Comparable<AnyType>> {
-    private static final ContactList contactList = new ContactList();
+class ContactListTest {
+    private static ContactList contactList = new ContactList();
 
     @Test
-    public void size_equals_CorrectInitialSizes() {
+    void size_equals_CorrectInitialSizes() {
         contactList.insert("Irene", "4152231231");
         assertEquals(5, contactList.size());
         assertEquals(2, contactList.numContactNodes());
         assertEquals(1, contactList.numContacts());
     }
 
-    @Test
-    public void size_equals_UpdatedSizesAfterRehash() {
-        contactList.insert("Jen", "4151112233");
-        assertEquals(11, contactList.size());
-        assertEquals(4, contactList.numContactNodes());
-        assertEquals(2, contactList.numContacts());
+    @ParameterizedTest
+    @ArgumentsSource(insertContacts.class)
+    void size_equals_UpdatedSizesAfterRehash(int expected, int actual) {
+        assertEquals(expected, actual);
     }
 
+    @ParameterizedTest
+    @ArgumentsSource(deleteContactsName.class)
+    void size_equals_CorrectSizesAfterDeleteName(int expected, int actual) {
+        assertEquals(expected, actual);
+    }
 
     @ParameterizedTest
-    @ArgumentsSource(searchAllContacts_SizeAndValues.class)
-    public void searchAllContacts_equals_ActualReturnMeetsExpected(AnyType expected,
-            AnyType actual) {
-        // refer to class to see test values
+    @ArgumentsSource(deleteContactsNumber.class)
+    void size_equals_CorrectSizesAfterDeleteNumber(int expected, int actual) {
         assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(searchAllContacts.class)
+    void searchAllContacts_equals_ActualReturnMeetsExpected(ArrayList<Contact> expected,
+            ArrayList<Contact> actual) {
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i).toString(), actual.get(i).toString());
+        }
+        assertEquals(expected.size(), actual.size());
     }
 
     @ParameterizedTest
     @ArgumentsSource(printAllContacts.class)
-    public void printAllContacts_equals_ReturnedContactsinOrder(AnyType expected, AnyType actual) {
-        contactList.insert("Isabell", "5101234556");
-        contactList.insert("Maria", "5105569987");
-        assertEquals(expected, actual);
-        assertNotEquals(makeContactList(makeContact("Isabell", "5101234556"),
-                makeContact("Jen", "4151112233"), makeContact("Maria", "5105569987"),
-                makeContact("Irene", "4152231231")), contactList.printAllContacts());
+    void printAllContacts_equals_ReturnedContactsInOrder(ArrayList<Contact> expected,
+            ArrayList<Contact> actual) {
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i).toString(), actual.get(i).toString());
+        }
+        assertEquals(expected.size(), actual.size());
+
     }
 
-    @Test
-    public void size_equals_CorrectSizesAfterDelete() {
-        contactList.delete("Irene");
-        assertEquals(11, contactList.size());
-        assertEquals(6, contactList.numContactNodes());
-        assertEquals(3, contactList.numContacts());
-        
-        contactList.insert("Evolet", "510-457-7760");
-        assertEquals(11, contactList.size());
-        assertEquals(8, contactList.numContactNodes());
-        assertEquals(4, contactList.numContacts());
-        
+    static class insertContacts implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext arg0)
+                throws Exception {
+            contactList.insert("Jen", "4151112233");
+            return Stream.of(Arguments.of(11, contactList.size()),
+                    Arguments.of(4, contactList.numContactNodes()),
+                    Arguments.of(2, contactList.numContacts()));
+        }
     }
-    
-    @Test
-    public void size_equals_CorrectSizesAfterDeletePart2() {
-        contactList.delete("4151112233");
-        assertEquals(11, contactList.size());
-        assertEquals(6, contactList.numContactNodes());
-        assertEquals(3, contactList.numContacts());
+
+    static class deleteContactsName implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext arg0)
+                throws Exception {
+            contactList.insert("Isabell", "5101234556");
+            contactList.insert("Maria", "5105569987");
+            contactList.delete("Irene");
+            return Stream.of(Arguments.of(23, contactList.size()),
+                    Arguments.of(6, contactList.numContactNodes()),
+                    Arguments.of(3, contactList.numContacts()));
+        }
     }
-    
+
+    static class deleteContactsNumber implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext arg0)
+                throws Exception {
+            contactList.delete("5101234556");
+            return Stream.of(Arguments.of(23, contactList.size()),
+                    Arguments.of(4, contactList.numContactNodes()),
+                    Arguments.of(2, contactList.numContacts()));
+        }
+    }
+
     private static Contact makeContact(String name, String number) {
         Contact contact = new Contact(name, number);
         return contact;
@@ -80,55 +103,37 @@ public class ContactListTest<AnyType extends Comparable<AnyType>> {
         for (Contact c : contacts) {
             arrayList.add(c);
         }
-
         return arrayList;
     }
 
-    public static class searchAllContacts_SizeAndValues implements ArgumentsProvider {
-
-        @Override
-        public Stream<? extends Arguments> provideArguments(ExtensionContext arg0)
-                throws Exception {
-            // TODO Auto-generated method stub
-            return Stream.of(Arguments.of(2, contactList.searchAllContacts("415").size()),
-                    Arguments.of(1, contactList.searchAllContacts("Ire").size()),
-                    Arguments.of(2, contactList.searchAllContacts("e").size()),
-                    Arguments.of(0, contactList.searchAllContacts("M").size()),
-                    Arguments.of(
-                            makeContactList(makeContact("Irene", "4152231231"),
-                                    makeContact("Jen", "4151112233")),
-                            contactList.searchAllContacts("415")),
-                    Arguments.of(makeContactList(makeContact("Irene", "4152231231")),
-                            contactList.searchAllContacts("Ire")),
-                    Arguments.of(
-                            makeContactList(makeContact("Irene", "4152231231"),
-                                    makeContact("Jen", "4151112233")),
-                            contactList.searchAllContacts("e")),
-                    Arguments.of(makeContactList(), contactList.searchAllContacts("M")),
-                    Arguments.of(
-                            makeContactList(makeContact("Irene", "4152231231"),
-                                    makeContact("Jen", "4151112233")),
-                            contactList.printAllContacts()));
-        }
-    }
-
-    public static class printAllContacts implements ArgumentsProvider {
-
+    static class searchAllContacts implements ArgumentsProvider {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext arg0)
                 throws Exception {
             // TODO Auto-generated method stub
             return Stream.of(
                     Arguments.of(makeContactList(makeContact("Irene", "4152231231"),
-                            makeContact("Isabell", "5101234556"),
-                            makeContact("Jen", "4151112233"), makeContact("Maria", "5105569987")),
-                            contactList.printAllContacts()),
-                    Arguments.of(
-                            makeContactList(makeContact("Irene", "4152231231"),
-                                    makeContact("Isabell", "5101234556"),
-                                    makeContact("Jen", "4151112233"),
-                                    makeContact("Maria", "5105569987")).size(),
-                            contactList.printAllContacts().size()));
+                            makeContact("Jen", "4151112233")),
+                            contactList.searchAllContacts("415")),
+                    Arguments.of(makeContactList(makeContact("Irene", "4152231231")),
+                            contactList.searchAllContacts("Ire")),
+                    Arguments.of(makeContactList(makeContact("Irene", "4152231231"),
+                            makeContact("Jen", "4151112233")),
+                            contactList.searchAllContacts("e")),
+                    Arguments.of(makeContactList(), contactList.searchAllContacts("M")));
+        }
+    }
+
+    static class printAllContacts implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext arg0)
+                throws Exception {
+            contactList.insert("Isabell", "5101234556");
+            contactList.insert("Rey", "4157752376");
+            return Stream.of(Arguments.of(makeContactList(makeContact("Irene", "4152231231"),
+                    makeContact("Isabell", "5101234556"), makeContact("Jen", "4151112233"),
+                    makeContact("Maria", "5105569987"), makeContact("Rey", "4157752376")),
+                    contactList.printAllContacts()));
         }
     }
 }
